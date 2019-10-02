66ee81e3312589fb4288199548d16f037dc31f97
sdob <sdob@cs.ucc.ie>
2019-10-02 11:27:00 +0100

    Set up basic Android platform-side messaging

---
+++ android/app/src/main/kotlin/com/masqt/diziet/Constants.kt
@@ -0,0 +1,8 @@
+package com.masqt.diziet
+
+object Constants {
+    const val CHANNEL_NAME_TELEPHONY = "com.masqt.diziet/telephony"
+
+    /* Client -> host messages */
+    const val MESSAGE_CLIENT_ACCEPT_INCOMING_CALL = "MESSAGE_CLIENT_ACCEPT_INCOMING_CALL"
+}
